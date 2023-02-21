9bfd64788fd1c3a4c4f2fac1cc981b54819bb0dc
taylorfturner <taylor.turner2@capitalone.com>
2023-02-21 12:55:01 -0500

    aggregator conf.py

--- docs/source/aggregator/conf.py
+++ docs/source/aggregator/conf.py
@@ -47,7 +47,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
-    'furo',
+    # 'furo',
     'sphinx.ext.intersphinx',
     "myst_parser",
 ]
