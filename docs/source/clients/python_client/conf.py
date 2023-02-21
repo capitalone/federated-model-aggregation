8dda1c83d61b0a7bca24674f2ea06b27c4fd238b
taylorfturner <taylor.turner2@capitalone.com>
2023-02-21 13:01:32 -0500

    conf python client

--- docs/source/clients/python_client/conf.py
+++ docs/source/clients/python_client/conf.py
@@ -40,7 +40,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
-    'furo',
+    # 'furo',
     'sphinx.ext.intersphinx',
     "myst_parser",
 ]
