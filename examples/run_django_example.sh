090f124e35f240f56fb847208557dfc689edd811
ksneab7 <ksneab7@gmail.com>
2023-04-17 08:46:08 -0400

    server full example spin up script added

---
+++ examples/run_django_example.sh
@@ -0,0 +1,9 @@
+cd django-deployment
+make compile-packages
+make install
+brew install redis
+pipenv run python3 manage.py migrate
+pipenv run python manage.py createsuperuser
+FMA_SETTINGS_MODULE=federated_learning_project.fma_settings pipenv run python manage.py runserver &
+redis-server &
+FMA_SETTINGS_MODULE=federated_learning_project.fma_settings pipenv run python manage.py qcluster & fg
