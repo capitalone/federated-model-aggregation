1bf2f013907880d766a51082e38e7f2c79a56b49
ksneab7 <91956551+ksneab7@users.noreply.github.com>
2023-08-17 14:10:48 -0400

    Revert "FMA | Javascript Client | Lint and Pre-Commit Setup (#37)" (#41)
    This reverts commit a57bcb091b4e71be8434a612f57bf7a810dffec4.

--- clients/javascript_client/fma_connect/settings.ts
+++ clients/javascript_client/fma_connect/settings.ts
@@ -1,6 +1,6 @@
 export class DefaultSettings {
-  url: string;
-  constructor() {
-    this.url = 'http://127.0.0.1:8000/';
-  }
+    url: string;
+    constructor(){
+        this.url = "http://127.0.0.1:8000/";
+    }
 }
